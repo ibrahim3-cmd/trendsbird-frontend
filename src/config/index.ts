67daa3bef5e0f7b54406291ce731947d6476c327
capture-grey <capturegrey@gmail.com>
2026-08-01 17:45:47 +0600

    isLive

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,16 @@
 import { isLive } from "@/constants/constant";
 
+const liveApiBaseUrl = "https://trendsbird-backend-1.onrender.com/api/v1";
+
 const config = {
   baseUrl: isLive
-    ? import.meta.env.VITE_BASE_URL_PROD || import.meta.env.VITE_BASE_URL
-    : import.meta.env.VITE_BASE_URL,
+    ? import.meta.env.VITE_BASE_URL_PROD || liveApiBaseUrl
+    : import.meta.env.VITE_BASE_URL || "http://localhost:5000/api/v1",
 };
 
 export const baseURL = isLive
-  ? import.meta.env.VITE_BASE_URL_PROD || import.meta.env.VITE_BASE_URL
-  : import.meta.env.VITE_BASE_URL;
+  ? import.meta.env.VITE_BASE_URL_PROD || liveApiBaseUrl
+  : import.meta.env.VITE_BASE_URL || "http://localhost:5000/api/v1";
 
 export const isLiveEnv = isLive;
 
